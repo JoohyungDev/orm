52d3acd3bd34460946f6f1525221576da5b50ec1
JooHyung <jhp21c@naver.com>
2024-03-19 21:05:46 +0900

    미들웨어 추가

--- Django/0319/jwt/config/settings.py
+++ Django/0319/jwt/config/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
