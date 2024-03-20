7882550251d40bfb698947aa5e887b48cf02ec92
JooHyung <jhp21c@naver.com>
2024-03-20 22:01:39 +0900

    회원가입 구현

--- Django/0319/insta/users/models.py
+++ Django/0319/insta/users/models.py
@@ -11,3 +11,9 @@ class User(AbstractUser):
     # 추가 필드 예시
     bio = models.TextField(blank=True)  # 사용자 기분 정보
     profile_picture = models.ImageField(upload_to="profile_pics/", blank=True)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics", blank=True, null=True)
--- Django/0319/insta/users/serializers.py
+++ Django/0319/insta/users/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
 
 User = get_user_model()
 
@@ -13,3 +16,39 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2")
+
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+        token = Token.objects.create(user=user)
+        return user, token
